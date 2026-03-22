78de07773a6fddacf3e1d91c866a55bf3b650d25
jianming <yeojm235@gmail.com>
2026-03-22 16:42:30 +0800

    Add javadoc comments

--- src/main/java/seedu/address/model/workout/WorkoutLog.java
+++ src/main/java/seedu/address/model/workout/WorkoutLog.java
@@ -1,22 +1,56 @@
 package seedu.address.model.workout;
 
-import java.util.Objects;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.person.Location;
 import seedu.address.model.person.Person;
 
+/**
+ * Represents a singular Workout Session.
+ * Guarantees: immutable
+ */
 public class WorkoutLog {
-    
+
     private final Person trainee;
     private final LocalDateTime time;
     private final Location location;
 
+    /**
+     * Constructs a WorkoutLog object. All fields must be non-null.
+     * @param trainee Person that performed the workout
+     * @param time Time during which the workout took place
+     * @param location Location of the Gym at which the workout took place
+     */
     public WorkoutLog(Person trainee, LocalDateTime time, Location location) {
+        requireAllNonNull(trainee, time, location);
         this.trainee = trainee;
         this.time = time;
         this.location = location;
+    }
+
+    /**
+     * Returns the Person that did the workout
+     */
+    public Person getTrainee() {
+        return trainee;
+    }
+
+    /**
+     * Returns the gym location where the workout took place
+     */
+    public Location getLocation() {
+        return location;
+    }
+
+    /**
+     * Returns the time during which the workout took place
+     */
+    public LocalDateTime getTime() {
+        return time;
     }
 
     @Override
