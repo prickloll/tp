af37789528ccd2258c9c5d25905369db3e43d073
Ryan Goh <ryangoh206@gmail.com>
2026-03-25 13:40:24 +0800

    Remove RowConstraints in PersonListCard and minor changes to MainWindow

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -18,8 +18,8 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * The Main Window. Provides the basic application layout containing
- * a menu bar and space where other JavaFX elements can be placed.
+ * The Main Window. Provides the basic application layout containing a menu bar and space where
+ * other JavaFX elements can be placed.
  */
 public class MainWindow extends UiPart<Stage> {
 
@@ -88,18 +88,15 @@ public class MainWindow extends UiPart<Stage> {
 
         /*
          * TODO: the code below can be removed once the bug reported here
-         * https://bugs.openjdk.java.net/browse/JDK-8131666
-         * is fixed in later version of SDK.
+         * https://bugs.openjdk.java.net/browse/JDK-8131666 is fixed in later version of SDK.
          *
-         * According to the bug report, TextInputControl (TextField, TextArea) will
-         * consume function-key events. Because CommandBox contains a TextField, and
-         * ResultDisplay contains a TextArea, thus some accelerators (e.g F1) will
-         * not work when the focus is in them because the key event is consumed by
-         * the TextInputControl(s).
+         * According to the bug report, TextInputControl (TextField, TextArea) will consume
+         * function-key events. Because CommandBox contains a TextField, and ResultDisplay contains
+         * a TextArea, thus some accelerators (e.g F1) will not work when the focus is in them
+         * because the key event is consumed by the TextInputControl(s).
          *
-         * For now, we add following event filter to capture such key events and open
-         * help window purposely so to support accelerators even when focus is
-         * in CommandBox or ResultDisplay.
+         * For now, we add following event filter to capture such key events and open help window
+         * purposely so to support accelerators even when focus is in CommandBox or ResultDisplay.
          */
         getRoot().addEventFilter(KeyEvent.KEY_PRESSED, event -> {
             if (event.getTarget() instanceof TextInputControl && keyCombination.match(event)) {
@@ -177,11 +174,12 @@ public class MainWindow extends UiPart<Stage> {
      */
     private CommandResult executeCommand(String commandText)
             throws CommandException, ParseException {
+        resultDisplay.setFeedbackToUser(commandText, IS_COMMAND_BOOLEAN);
         try {
             CommandResult commandResult = logic.execute(commandText);
-            resultDisplay.setFeedbackToUser(commandText, IS_COMMAND_BOOLEAN);
             logger.info("Result: " + commandResult.getFeedbackToUser());
-            resultDisplay.setFeedbackToUser(commandResult.getFeedbackToUser(), IS_NOT_COMMAND_BOOLEAN);
+            resultDisplay.setFeedbackToUser(commandResult.getFeedbackToUser(),
+                    IS_NOT_COMMAND_BOOLEAN);
 
             if (commandResult.isShowHelp()) {
                 handleHelp();
