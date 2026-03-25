e9a236773c2e54323c85a25c32bb0456b8da3bcb
jianrontan <116509027+jianrontan@users.noreply.github.com>
2026-03-25 13:01:19 +0800

    Add enhanced help command with specific command support

---
+++ src/test/java/seedu/address/logic/parser/HelpCommandParserTest.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.HelpCommand;
+
+public class HelpCommandParserTest {
+
+    private HelpCommandParser parser = new HelpCommandParser();
+
+    @Test
+    public void parse_emptyArg_returnsHelpCommand() {
+        // empty string
+        HelpCommand expectedHelpCommand = new HelpCommand("");
+        assertParseSuccess(parser, "", expectedHelpCommand);
+
+        // whitespace only
+        assertParseSuccess(parser, "     ", expectedHelpCommand);
+    }
+
+    @Test
+    public void parse_validArgs_returnsHelpCommand() {
+        // single command word
+        HelpCommand expectedHelpCommand = new HelpCommand("add");
+        assertParseSuccess(parser, "add", expectedHelpCommand);
+
+        // command word with leading and trailing whitespaces
+        assertParseSuccess(parser, "  add  ", expectedHelpCommand);
+
+        // different command word
+        expectedHelpCommand = new HelpCommand("delete");
+        assertParseSuccess(parser, "delete", expectedHelpCommand);
+    }
+}
