a2d60f8ef52f9041405c293bf8150cdfd8edb79f
Ryan Goh <ryangoh206@gmail.com>
2026-04-02 19:58:20 +0800

    Update remarks regarding noteCommand in UG and messages in NoteCommand

--- src/main/java/seedu/address/logic/commands/NoteCommand.java
+++ src/main/java/seedu/address/logic/commands/NoteCommand.java
@@ -21,8 +21,8 @@ public class NoteCommand extends Command {
     public static final String COMMAND_WORD = "note";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Adds or appends a note to the specified person by index number used "
-            + "in the displayed person list.\n"
+            + ": Adds or appends a note to the specified client by index number used "
+            + "in the displayed client list.\n"
             + "Use 'n/' to replace/delete existing notes, or 'a/' to append to existing notes.\n"
             + "Only one prefix (n/ or a/) should be provided, not both.\n"
             + "Parameters: INDEX (must be a positive integer) [n/NOTE_CONTENT | a/NOTE_CONTENT]\n"
@@ -30,11 +30,11 @@ public class NoteCommand extends Command {
             + " 1 n/Focuses on strength more. (replaces existing note)\n" + "  " + COMMAND_WORD
             + " 1 a/Goes to gym daily. (appends to existing note)";
 
-    public static final String MESSAGE_ADD_SUCCESS = "Note added to person: %1$s";
-    public static final String MESSAGE_APPEND_SUCCESS = "Note appended to person: %1$s";
+    public static final String MESSAGE_ADD_SUCCESS = "Note added to client: %1$s";
+    public static final String MESSAGE_APPEND_SUCCESS = "Note appended to client: %1$s";
     public static final String MESSAGE_NOCHANGE_SUCCESS =
-            "No changes made to note for person: %1$s";
-    public static final String MESSAGE_DELETE_SUCCESS = "Note deleted from person: %1$s";
+            "No changes made to note for client: %1$s";
+    public static final String MESSAGE_DELETE_SUCCESS = "Note deleted from client: %1$s";
     public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Note: %2$s";
 
     private final Index index;
