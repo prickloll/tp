9ea0088930c4e786d45489bca8234748c9459480
jianming <yeojm235@gmail.com>
2026-04-06 14:40:21 +0800

    Add reset logs test cases

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalWorkoutLogs.getTypicalWorkoutLogBook;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,6 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.WorkoutLogBook;
 
 public class ClearCommandTest {
 
@@ -23,9 +23,10 @@ public class ClearCommandTest {
 
     @Test
     public void execute_nonEmptyAddressBook_success() {
-        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs(), new WorkoutLogBook());
-        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs(), new WorkoutLogBook());
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs(), getTypicalWorkoutLogBook());
+        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs(), getTypicalWorkoutLogBook());
         expectedModel.setAddressBook(new AddressBook());
+        expectedModel.resetLogs();
 
         assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
